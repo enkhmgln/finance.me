5866e85eb4921e649c59f155b1b184daf3b4459c
enkhmgln <sad64137@gmail.com>
2024-01-01 19:46:12 +0800

    constants , predefined constants , magic constants , require hiisen file dotor huwisagch oruulah

--- constants.php
+++ constants.php
@@ -1,10 +1,19 @@
 <?php
-
+// const буюу дахин өөрчлөгдөхгүй хувьсагчийг зарлахдаа дараах түлхүүр үгийг ашиглана. define функц дотор 2 argument өгдөг ба
+// эхний argument дээр const нэр 2 дах argument дээр value
 define('NUM', 12);
-echo NUM;
+// echo NUM;
 const NUM2 = 1234;
-echo NUM2;
+// echo NUM2;
+// Predefined constants буюу урьдчилан зарласан php аль хэдийн байдаг constants юм.
 echo '<br>Миний ашиглаж буй php хувилбар : ' . PHP_VERSION;
 echo '<br>Миний php хэлний байршиж буй хавтасны зам  : ' . PHP_LIBDIR;
-// ОДоо байгаа замыг харуулна.
+// Magic constant - ууд эхлэхдээ доогуур 2 зураасаар эхэлдэг байна.
+
+// ОДоо байгаа directory буюу folder - ийн замыг харуулдаг MAGIC CONSTANT.
 echo '<br>' . __DIR__;
+// ямар file дээр ажиллаж байгаа замыг харуулдаг MAGIC CONSTANT
+echo __FILE__;
+// Тухайн file - дотор LINE constant - ийг бичихэд аль мөр дээр байгааг зааж өгнө.
+// Жишээ :
+echo '<BR>Энэ файлын кодын мөрийн тоо : ' . __LINE__;
